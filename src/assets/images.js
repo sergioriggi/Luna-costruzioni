c018d07c237cdc7b416f9472c7de990da98053e3
Sergio Riggi <sergioriggi@me.com>
2026-08-26 11:54:42 +0100

    Initial commit: architettura React per Luna Costruzioni

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,13 +1,13 @@
 // Image references for Hero and Gallery
-export const HERO = '/foto/Immagine%20WhatsApp%202025-07-24%20ore%2020.09.48_af48d0ac.jpg'
+export const HERO = '/catalogo/0001.jpg'
 
 export const GALLERY = [
-    '/foto/Immagine%20WhatsApp%202025-07-24%20ore%2020.06.51_a269a728.jpg',
-    '/foto/Immagine%20WhatsApp%202025-07-24%20ore%2020.09.48_af48d0ac.jpg',
+    '/catalogo/0002.jpg',
+    '/catalogo/0003.jpg',
+    '/catalogo/0004.jpg',
     '/catalogo/0005.jpg',
-    '/catalogo/0001.jpg',
-    '/foto/Immagine%20WhatsApp%202025-07-25%20ore%2009.48.56_b3ca90f8.jpg',
-    '/foto/Immagine%20WhatsApp%202025-07-25%20ore%2009.48.49_70b91350.jpg'
+    '/catalogo/0006.jpg',
+    '/catalogo/0007.jpg'
 ]
 
 export default { HERO, GALLERY }
